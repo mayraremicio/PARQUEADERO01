59eb7e0c47fe944918317fbc6dc3b625de5c6b76
AleGarcia12335 <112192213+AleGarcia12335@users.noreply.github.com>
2023-10-17 17:34:24 -0500

    cambios al parqueadero de arreglo mas ciclo for

--- src/Main.java
+++ src/Main.java
@@ -16,12 +16,17 @@ class main{
           String placa;
           String nombre;
           double horaInicio,horasalida,valor1,resta1,valor2,vehiculo;
-          int moto=1000,carro=2000;
+          int moto=1000,carro=2000,nvehiculos;
+
+
+
 
 
           JOptionPane.showInternalMessageDialog(null,"Bienvenidos al parqueadero");
           vehiculo =Integer.parseInt(JOptionPane.showInputDialog ("escriba el 1 si su vehiculo es un moto o el 2 si es un carro"));
-          placa =JOptionPane.showInputDialog(" ingrese la placa del vehiculo:");
+
+           placa =JOptionPane.showInputDialog(" ingrese la placa del vehiculo:");
+
           nombre = JOptionPane.showInputDialog(" ingrese el nombre del conductor:");
 
           horaInicio = java.lang.Double.parseDouble(JOptionPane.showInputDialog("ingrese la hora de llegada:"));
@@ -60,37 +65,49 @@ class main{
 
                JOptionPane.showInternalMessageDialog(null,"horas que estuvo en el parqueadero:" + resultado);}*/
 
-          switch ((int) vehiculo){
+          switch ((int) vehiculo) {
                case 1:
-                    resta1 = horasalida - horaInicio;
+                    resta1=horasalida - horaInicio;
 
-                    double numero = resta1;
-                    long resultado = Math.round( numero );
+                    double numero=resta1;
+                    long resultado=Math.round(numero);
 
-                    valor2 = resultado*moto;
+                    valor2=resultado * moto;
 
-                    JOptionPane.showInternalMessageDialog(null,"el valor a pagar es: " + valor2);
+                    JOptionPane.showInternalMessageDialog(null, "el valor a pagar es: " + valor2);
 
-                    JOptionPane.showInternalMessageDialog(null,"horas que estuvo en el parqueadero: " + resultado);
+                    JOptionPane.showInternalMessageDialog(null, "horas que estuvo en el parqueadero: " + resultado);
                     break;
                case 2:
-                    resta1 = horasalida - horaInicio;
+                    resta1=horasalida - horaInicio;
 
-                    double numer = resta1;
-                    long resultad = Math.round( numer );
+                    double numer=resta1;
+                    long resultad=Math.round(numer);
 
-                    valor2 = resultad*carro;
+                    valor2=resultad * carro;
 
-                    JOptionPane.showInternalMessageDialog(null,"el valor a pagar es: " + valor2);
+                    JOptionPane.showInternalMessageDialog(null, "el valor a pagar es: " + valor2);
 
-                    JOptionPane.showInternalMessageDialog(null,"horas que estuvo en el parqueadero: " + resultad);
+                    JOptionPane.showInternalMessageDialog(null, "horas que estuvo en el parqueadero: " + resultad);
+                    int [] ncarros= new int [10];
+
+
+                    JOptionPane.showMessageDialog(null, "registro de carros en el parqueadero");
+
+                    for (int i=0; i < 10; i++) ;
+
+                    JOptionPane.showMessageDialog(null, "carros en el parqueadero");
+
+                    for (int i=0 ; i <10; i++);{
+
+
+                    int i;
+                    JOptionPane.showMessageDialog(null, " carro" + (i + 1) + ":");+placa [i];
+
                     break;
 
-
-               default: JOptionPane.showInternalMessageDialog(null, "no aceptamos ese tipo de vehiculos");
-
-
-
+               default:
+                    JOptionPane.showInternalMessageDialog(null, "no aceptamos ese tipo de vehiculos");
 
 
 
@@ -102,5 +119,5 @@ class main{
           }
 
 
-    }
-}
+
+     }
